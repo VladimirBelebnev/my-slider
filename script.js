ad676301f84cdc0d9f26c86a32752918ca918aba
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-02-25 15:51:44 +0300

    add slider and settings
    add slider and settings

---
+++ script.js
@@ -0,0 +1,12 @@
+import slider from '/';
+
+slider({
+    container: '',
+    nextArrow: '',
+    prevArrow: '',
+    slide: '',
+    totalCounter: '',
+    currentCounter: '',
+    wrapper: '',
+    field: '',
+});
